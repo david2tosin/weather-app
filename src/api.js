de939c1d16516ae51c0863ab44ad4495661cdf49
Oluwatosin Oduwole <david2tosin@gmail.com>
2023-02-18 21:33:25 +0100

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ca5c588c53mshcc88fd2ae4037ecp1fdc2fjsn0c5936d777c7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "909be6197755b20836bf5bf4cfc7d92e";
